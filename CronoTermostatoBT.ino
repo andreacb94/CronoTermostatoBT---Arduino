99f9c4d9187478ffe5c6a3741579fe75b383af4e
andreacb94 <andreacb94@users.noreply.github.com>
2017-11-12 20:18:26 +0100

    Initial commit
    ITA: Creazione del progetto.
    ENG: Created the project

---
+++ CronoTermostatoBT.ino
@@ -0,0 +1,17 @@
+/** CREATED BY ANDREA PETRELLA.
+ *  DATE: 12/11/2017
+ */
+
+/*** Progetto per usare Arduino come crono-termostato per caldaia con interfaccia Bluetooth per dispositivi mobile ***/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
